18bdeeca17e37befbd0c6b5aebadddc547af3f6b
billy <billy@bob.com>
2013-07-05 13:02:51 -0700

    made changes to ioc tests and container controller

--- IocExample/IocExample.Tests/IocExampleTests.cs
+++ IocExample/IocExample.Tests/IocExampleTests.cs
@@ -3,32 +3,98 @@ using NUnit.Framework;
 
 namespace IocExample.Tests
 {
-
     [TestFixture]
     public class IocExampleTests
     {
+        Container container;
+
         [SetUp]
-        public void init()
+        public void Init()
         {
-            Container container = new Container();
+            //Arrange
+            container = new Container();
         }
 
         [Test]
+        public void CanCreateAContainer()
+        {
+            //Assert
+            Assert.IsNotNull(container);
+        }
+
+        [Test]
+        public void CanRegisterAndResolveTypes()
+        {
+            //Arrange
+            container.Register<IRepository, Repository>();
+
+            //Act
+            IRepository repository = container.Resolve<IRepository>();
+
+            //Assert
+            Assert.IsNotNull(repository);
+        }
+
+        [Test]
+        [ExpectedException(typeof(UnknownTypeException))]
         public void UnknownTypeThrowsException()
         {
-            Assert.AreEqual(1, 1);
-            //container.Register<IRepository, Repository>();
-            //IRepository repository = Container.Resolve<IRepository>();
-            //Assert.AreEqual(Container.Resolve<UnknownType>());
+            //Act
+            IRepository repository = container.Resolve<IRepository>();
         }
 
         [Test]
-        public void ContainerCanRegisterTypes()
+        [ExpectedException(typeof(TypeExistsException))]
+        public void DuplicateTypeThrowsException()
         {
-            //Container container = new Container();
-            //container.Register<IRepository, Repository>();
-            //IRepository repository = container.Resolve<IRepository>();
-            //Assert.IsNotNull(repository);
+            //Arrange
+            container.Register<IRepository, Repository>();
+            container.Register<IRepository, Repository>();
+        }
+
+        [Test]
+        public void ContainerDefaultIsTransient()
+        {
+            //Arrange
+            container.Register<IRepository, Repository>();
+
+            //Act
+            IRepository one = container.Resolve<IRepository>();
+            IRepository two = container.Resolve<IRepository>();
+
+            //Assert
+            Assert.AreNotSame(one, two);
+
+            //Could I check the iocObject._type against the LifestyleType enum?  perhaps...
+            //Assert.AreEqual(container.iocObjects[Instance]._type, LifestyleType.Transient);
+        }
+
+        [Test]
+        public void SingletonReturnsSameInstance()
+        {
+            //Arrange
+            container.Register<IRepository, Repository>(LifestyleType.Singleton);
+
+            //Act
+            IRepository one = container.Resolve<IRepository>();
+            IRepository two = container.Resolve<IRepository>();
+
+            //Assert
+            Assert.AreSame(one, two);
+        }
+
+        [Test]
+        public void TransientReturnsDifferentInstance()
+        {
+            //Arrange
+            container.Register<IRepository, Repository>();
+
+            //Act
+            IRepository one = container.Resolve<IRepository>();
+            IRepository two = container.Resolve<IRepository>();
+
+            //Assert
+            Assert.AreNotSame(one, two);
         }
     }
 }
--- IocExample/IocExample/Custom/ContainerController.cs
+++ IocExample/IocExample/Custom/ContainerController.cs
@@ -8,60 +8,62 @@ using System.Web.Routing;
 
 namespace IocExample
 {
-    //public class ContainerControllerFactory : DefaultControllerFactory
-    //{
-    //    private readonly Container _container;
+    /*
+    public class ContainerControllerFactory : DefaultControllerFactory
+    {
+        private readonly Container _container;
 
-    //    private ContainerControllerFactory()
-    //    {
-    //    }
+        private ContainerControllerFactory()
+        {
+        }
 
-    //    public ContainerControllerFactory(Container container)
-    //    {
-    //        _container = container;
-    //    }
+        public ContainerControllerFactory(Container container)
+        {
+            _container = container;
+        }
 
-    //    protected override Type GetControllerType(RequestContext requestContext, string controllerName)
-    //    {
-    //        return base.GetControllerType(requestContext, controllerName);
-    //    }
+        protected override Type GetControllerType(RequestContext requestContext, string controllerName)
+        {
+            return base.GetControllerType(requestContext, controllerName);
+        }
 
-    //    //why can't I override this??
-    //    protected internal override IController GetControllerInstance(RequestContext requestContext, Type controllerType)
-    //    {
-    //        if (controllerType == null)
-    //            throw new HttpException(404, "Page not found");
+        //why can't I override this??
+        protected internal override IController GetControllerInstance(RequestContext requestContext, Type controllerType)
+        {
+            if (controllerType == null)
+                throw new HttpException(404, "Page not found");
 
-    //        if (!typeof(IController).IsAssignableFrom(controllerType))
-    //            throw new ArgumentException("Type does not subclass IController", "controllerType");
+            if (!typeof(IController).IsAssignableFrom(controllerType))
+                throw new ArgumentException("Type does not subclass IController", "controllerType");
 
-    //        object[] parameters = null;
+            object[] parameters = null;
 
-    //        ConstructorInfo constructor = controllerType.GetConstructors().FirstOrDefault(c => c.GetParameters().Length > 0);
-    //        if (constructor != null)
-    //        {
-    //            ParameterInfo[] parametersInfo = constructor.GetParameters();
-    //            parameters = new object[parametersInfo.Length];
+            ConstructorInfo constructor = controllerType.GetConstructors().FirstOrDefault(c => c.GetParameters().Length > 0);
+            if (constructor != null)
+            {
+                ParameterInfo[] parametersInfo = constructor.GetParameters();
+                parameters = new object[parametersInfo.Length];
 
-    //            for (int i = 0; i < parametersInfo.Length; i++)
-    //            {
-    //                ParameterInfo parameter = parametersInfo[i];
+                for (int i = 0; i < parametersInfo.Length; i++)
+                {
+                    ParameterInfo parameter = parametersInfo[i];
 
-    //                if (_container.typeExists(parameter) == null)
-    //                    throw new ApplicationException("Could not create controller");
+                    if (_container.typeExists(parameter) == null)
+                        throw new ApplicationException("Could not create controller");
 
-    //                parameters[i] = _container.Resolve<parameter>;
-    //            }
-    //        }
+                    parameters[i] = _container.Resolve<parameter>;
+                }
+            }
 
-    //        try
-    //        {
-    //            return (IController) Activator.CreateInstance(controllerType, parameters);
-    //        }
-    //        catch (Exception ex)
-    //        {
-    //            throw new InvalidOperationException("Could not create controller", ex);
-    //        }
-    //    }
-    //}
+            try
+            {
+                return (IController)Activator.CreateInstance(controllerType, parameters);
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException("Could not create controller", ex);
+            }
+        }
+    }
+     * */
 }
